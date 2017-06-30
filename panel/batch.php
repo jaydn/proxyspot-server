2bacd5fd3cf13a544fb1b1be82e0eb96ecb61454
Jaydn Cunningham <jaydn@gmx.us>
2017-06-30 16:32:59 +0930

    reuse vars

--- panel/batch.php
+++ panel/batch.php
@@ -17,15 +17,21 @@
             <div class="row text-center">
                 <p>
                 <?php
-                    $yourbatch = $cdb->OwnerOfBatchcode($_GET['b']) == $_SESSION['userid'];
-                    $public = $cdb->IsPublicBatchcode($_GET['b']);
-                    if(!$cdb->UsedBatchcode($_GET['b']) || !($yourbatch || $public)) {
+                    $batchcode = $_GET['b'];
+
+                    $owner = $cdb->OwnerOfBatchcode($batchcode) == $_SESSION['userid'];
+                    $public = $cdb->IsPublicBatchcode($batchcode);
+                    $used = $cdb->UsedBatchcode($batchcode);
+
+                    if(!$used || !($owner || $public)) {
                         readfile('reuse/bad_batch.html');
                     }
-                    echo '<p>'.htmlspecialchars($_GET['b']).'</p>';
-                    if($cdb->OwnerOfBatchcode($_GET['b']) == $_SESSION['userid']) {
-                        echo '<a href="publicity.php?b='.htmlspecialchars($_GET['b']).'"><button>';
-                        echo $cdb->IsPublicBatchcode($_GET['b']) ? 'public' : 'private';
+
+                    $san_batchcode = htmlspecialchars($batchcode);
+                    echo '<p>'.$san_batchcode.'</p>';
+                    if($owner) {
+                        echo '<a href="publicity.php?b='.$san_batchcode.'"><button>';
+                        echo $public ? 'public' : 'private';
                         echo '</button></a>'."\n";
                     }
                 ?>
@@ -42,16 +48,25 @@
                     </thead>
                     <tbody>
                         <?php
-                        if($cdb->UsedBatchcode($_GET['b'])) {
-                            if($cdb->IsPublicBatchcode($_GET['b']) || $cdb->OwnerOfBatchcode($_GET['b']) == $_SESSION['userid']) {
+                        if($used) {
+                            if($public || $owner) {
                                 $stmt = $cdb->db->prepare('SELECT * FROM proxies WHERE batchcode=?');
-                                $stmt->execute([$_GET['b']]);
+                                $stmt->execute([$batchcode]);
                                 foreach($stmt as $row) {
+                                    $differentexit = (strcmp($row['exitip'], $row['entryip']) != 0);
                                     echo '<tr>'."\n";
                                     echo '<td>'.$row['entryip'].'</td>'."\n";
                                     echo '<td>'.$row['entryport'].'</td>'."\n";
                                     echo '<td>'.$row['entrytype'].'</td>'."\n";
-                                    echo '<td>'.$row['exitip'].'</td>'."\n";
+                                    echo '<td>';
+                                    if($differentexit) {
+                                        echo '<span style="background-color:#FF8080;">';
+                                    }
+                                    echo $row['exitip'];
+                                    if($differentexit) {
+                                        echo '</span>';
+                                    }
+                                    echo '</td>'."\n";
                                     echo '</tr>'."\n";
                                 }
                             }
