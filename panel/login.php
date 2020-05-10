2be4853a72957df0e557476b520057a0d275734c
Jaydn Cunningham <jaydn@gmx.us>
2020-05-11 00:28:15 +0930

    begin conversion to twig templates, fix panel redirect bug

--- panel/login.php
+++ panel/login.php
@@ -1,45 +1,46 @@
 <?php
-    require_once('reuse/perusal_sesh.php');
-    require_once('reuse/login_funcs.php');
-    if(isset($_SESSION['userid'])) {
-        header('Location: panel.php');
-    }
+require_once('reuse/perusal_sesh.php');
+require_once('reuse/login_funcs.php');
+if (isset($_SESSION['userid'])) {
+    header('Location: panel.php');
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <?php
-        readfile('reuse/head.html');
-    ?>
-    
-    <body>
-        <div class="container">
-            <?php
-                readfile('reuse/banner.html');
-            ?>
-            <br>
-            <div class="row">
-                <div class="jumbotron">
-                    <?php
-                        $login = new CLogin();
-                        if($login->IsLoginAttempt()) {
-                            switch($login->Login($_POST['u'], $_POST['p'])) {
-                                case 0:
-                                    readfile('reuse/login_form/valid_login.html');
-                                    break;
-                                case 1:
-                                    readfile('reuse/login_form/bad_password.html');
-                                    readfile('reuse/login_form/login_form.html');
-                                    break;
-                                default:
-                                    readfile('reuse/login_form/unk_error.html');
-                                    break;
-                            }
-                        } else {
+<?php
+readfile('reuse/head.html');
+?>
+
+<body>
+    <div class="container">
+        <?php
+        readfile('reuse/banner.html');
+        ?>
+        <br>
+        <div class="row">
+            <div class="jumbotron">
+                <?php
+                $login = new CLogin();
+                if ($login->IsLoginAttempt()) {
+                    switch ($login->Login($_POST['u'], $_POST['p'])) {
+                        case 0:
+                            readfile('reuse/login_form/valid_login.html');
+                            break;
+                        case 1:
+                            readfile('reuse/login_form/bad_password.html');
                             readfile('reuse/login_form/login_form.html');
-                        }
-                    ?>
-                </div>
+                            break;
+                        default:
+                            readfile('reuse/login_form/unk_error.html');
+                            break;
+                    }
+                } else {
+                    echo $twig->render('test.html', ['name' => 'Fabien']);
+                }
+                ?>
             </div>
         </div>
-   </body>
+    </div>
+</body>
+
 </html>
--- panel/reuse/perusal_sesh.php
+++ panel/reuse/perusal_sesh.php
@@ -1,4 +1,8 @@
 <?php
     session_name('perusal_sesh');
     session_start();
+
+    require_once('../vendor/autoload.php');
+    $loader = new \Twig\Loader\FilesystemLoader('templates');
+    $twig = new \Twig\Environment($loader);
 ?>
